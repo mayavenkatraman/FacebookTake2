7b711bae105d3d45864ec793b71d89085d55e2cb
mayav <mayav@yahoo-inc.com git config --global user.name mayavenkaraman git config --global user.email mayav@yahoo-inc.com>
2014-09-07 00:01:28 -0700

    test
    zxz

---
+++ FacebookTake2/CameraViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CameraViewController.swift
+//  FacebookTake2
+//
+//  Created by Maya Venkatraman on 9/6/14.
+//  Copyright (c) 2014 Maya Venkatraman. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func dismiss(sender: AnyObject) {
+        dismissViewControllerAnimated(false, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
